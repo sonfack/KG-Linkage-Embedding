817277643ae1ade6e088d0bb500ba6bca06cf569
SONFAK S. Serge <sss.sonfack@gmail.com>
2019-05-17 15:32:50 +0200

    looping on kg

--- src/kgmanagement.py
+++ src/kgmanagement.py
@@ -5,10 +5,18 @@ from src.commons import DATA_FOLDER, KB_FOLDER
 """
 Put you KG files in the data folder
 """
+DESCRIPTION = "description"
+HAS_TIGR_IDENTIFIER = "has_tigr_identifier"
+LABEL = "label"
+HAS_UNIPROT_ASSESSION = "has_uniprot_assession"
+
 
 def getEntitiesPropertiesValue(kgFileName):
     completeKgFileName = os.path.join(KB_FOLDER, kgFileName)
     g = Graph()
     result = g.parse(completeKgFileName, format="n3")
     for s, p, o in g:
-        print(p)
+        graphProperty = s
+        graphProperties = graphProperties.split("/")
+        if LABEL in graphProperties[-1]:
+            pass 
