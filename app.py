beeb7a0dcdf0029333a3acebfdb377a2fa7222ad
SONFAK S. Serge <sss.sonfack@gmail.com>
2019-05-21 10:23:37 +0200

    stable retrieve of entities properties values and save in tab seperated file

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-from src.commons import readDataFile
+from src.commons import readDataFile, readUnformatedFile
 from src.kgmanagement import getEntitiesPropertiesValue
 
 if __name__== "__main__":
+   # create extracted values from predicates of all entities in the file
    getEntitiesPropertiesValue("gramene_Oryza_sativa_japonica_genes.ttl")
-   exit()
-   dFille = readDataFile("reference.table.txt")
-   print(dFille)
+   
+   """
+   dFille = readDataFile("reference.txt", folder="Texts")
+   #dFille = readDataFile("gramene_Oryza_sativa_japonica_genes.csv", folder="Output")
+   print(dFille.head())
+   """
+   
+   
--- src/kgmanagement.py
+++ src/kgmanagement.py
@@ -9,7 +9,7 @@ Bellow are the properties we are looking on each entity
 DESCRIPTION = "description"
 HAS_TIGR_IDENTIFIER = "has_tigr_identifier"
 LABEL = "label"
-HAS_UNIPROT_ASSESSION = "has_uniprot_assession"
+HAS_UNIPROT_ASSESSION = "has_uniprot_accession"
 
 """
 2. This function outputs a frame containing values of selected attributs of a given graph
@@ -22,35 +22,68 @@ def getEntitiesPropertiesValue(kgFileName, properties=None):
     else:
         listOfProperties = properties
     listOfProperties.insert(0, "entity" )
-    outputList = {}
+    listOfProperties.sort()
+    print("#############################################################################################################################")
+    print(listOfProperties)
+    print("#############################################################################################################################")
     # Output file of values of relevent properties of each entity
-    outputFile = os.path.join(OUTPUT, kgFileName)
+    kgFileName = kgFileName.split(".")
+    outputFile = os.path.join(OUTPUT, kgFileName[0]+".csv")
     graphFile = open(outputFile, "a+")
     graphFile.write("\t".join(listOfProperties))
+    graphFile.write("\n")
     graphFile.close()
-    listOfProperties.pop(0)
-    completeKgFileName = os.path.join(KB_FOLDER, kgFileName)
+    completeKgFileName = os.path.join(KB_FOLDER, ".".join(kgFileName))
     g = Graph()
     result = g.parse(completeKgFileName, format="n3")
     listOfSubjectsInGraph = g.subjects()
     for s in listOfSubjectsInGraph:
+        outputList = {}
+        print("subject",s)
         listOfPropertiesInSubject = g.predicates(subject=s)
         for p in listOfPropertiesInSubject:
             graphProperty = p
+            print(graphProperty)
             graphProperty = graphProperty.split("/")
-            for givenProperty in listOfProperties:
-                if  graphProperty[-1] in givenProperty:
-                    listOfObjects = g.objects(subject=s, predicate=p)
-                    res = [obj for obj in listOfObjects]
-                    outputList[givenProperty]="$$".join(res)
+            nameOfProperty = graphProperty[-1]
+            if  nameOfProperty in listOfProperties or nameOfProperty.find("#label") != -1:
+                listOfObjects = g.objects(subject=s, predicate=p)
+                res = [obj for obj in listOfObjects]
+                if len(res) > 0:
+                    if nameOfProperty.find("#label") != -1:
+                        outputList["label"] = " ".join(res)
+                    else:
+                        outputList[nameOfProperty] = " ".join(res)
                 else:
-                    outputList[givenProperty]=" "
-        print(outputList)
-        saveEntityAsFrameInFile(outputFile, listOfProperties, s, outputList)
+                    outputList[nameOfProperty] = " "
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+                print(outputList)
+                print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+        saveEntityAsFrameInFile(outputFile, s, outputList,listOfProperties)
 
-
-
-def saveEntityAsFrameInFile(outputFile, listOfProperties, entityName, outputList):
+        
+"""
+This function saves values of properties in the file given as parameter
+"""        
+def saveEntityAsFrameInFile(outputFile, entity, outputList, listOfProperties):
+    outputListSorted = []
     graphFile = open(outputFile, "a+")
-    graphFile.write(str(entityName)+"\t"+"\t".join(str(objectValue) for objectValue in outputList.values()))
+    outputList["entity"] = entity.split("/")[-1]
+    print("#############################################################################################################################")
+    if len(outputList) != len(listOfProperties):
+        for property in listOfProperties:
+            if property not in outputList.keys():
+                outputList[property]=" "
+    print(outputList)
+    print("#############################################################################################################################")
+    for key in sorted(outputList.keys()):
+        propertyValue = outputList[key].split()
+        if len(propertyValue)>= 2:
+            listPropertyValue = [theValue.split("/")[-1] for theValue in propertyValue]
+            outputListSorted.append(" ".join(listPropertyValue))
+        else:
+            outputListSorted.append(" ".join(propertyValue))
+    linesInFile = "\t".join(outputListSorted)
+    graphFile.write(linesInFile)
+    graphFile.write("\n")
     graphFile.close()
