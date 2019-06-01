c201429230f5ed599b1636e89abf502060ba8d77
SONFAK S. Serge <sss.sonfack@gmail.com>
2019-06-01 17:54:28 +0200

    add the test folder and file for unit testing

---
+++ test/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestLinkage(unittest.TestCase):
+    def testCooccurentMat(self):
+        pass 
+
+
+if __name__=="__main__":
+    myTest = TestLinkage()
+    myTest.testCooccurentMat()
