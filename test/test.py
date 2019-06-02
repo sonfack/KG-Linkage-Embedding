5303dddef609b6cee03e4eae2234ed789a9dadff
SONFAK S. Serge <sss.sonfack@gmail.com>
2019-06-02 21:00:24 +0200

    impplementation of test for kmeans and centroid

--- test/test.py
+++ test/test.py
@@ -1,11 +1,34 @@
 import unittest
-
+import numpy as np 
+from src.commons import findElementInListOfList, kMeans, calculateCenter
 
 class TestLinkage(unittest.TestCase):
-    def testCooccurentMat(self):
-        pass 
+    
+    def test_FindElementInListOfList(self):
+        L = [[3,4,6],[1,8],[3,7,9]]
+        e = 9
+        self.assertTrue(findElementInListOfList(L,e))
+
+    def test_kMeans(self):
+        listOfVectors = [[1,1], [5,3], [2,1], [4,3], [5,4],[4,4]]
+        k = 2
+        kmeans = kMeans(listOfVectors, k)
+        if k >= 6:
+            self.assertEqual(kmeans, [])
+        else:
+            print(kmeans)
+            self.assertTrue(kmeans[0] == [0,2] and kmeans[1] == [1,3,4,5], 'Bad classification')
+        
+    def test_calculateCenter(self):
+        listOfVectors = [[1,1], [5,3], [2,1], [4,3], [5,4],[4,4]]
+        listOfPoints = [0,3]
+        self.assertTrue(np.all(calculateCenter(listOfPoints, listOfVectors) == [(1+4)/2,(1+3)/2]))
 
 
+
+    
 if __name__=="__main__":
     myTest = TestLinkage()
-    myTest.testCooccurentMat()
+    #myTest.test_FindElementInListOfList()
+    #myTest.test_kMeans()
+    myTest.test_calculateCenter()
